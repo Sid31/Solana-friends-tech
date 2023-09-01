61f941c0ee481ac98d056b752b024dc3dce66d10
sidou <sid.berraf@gmail.com>
2023-09-01 13:53:32 +0800

    removing useless file in the wrong project

--- src/friendTech/solanaFriendTech.rs
+++ src/friendTech/solanaFriendTech.rs
@@ -1,27 +1,27 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
-    decode_error::DecodeError,
     entrypoint,
-    program::{invoke, invoke_signed},
+    program::{invoke},
     program_error::ProgramError,
     program_pack::Pack,
     pubkey::Pubkey,
-    sysvar::{clock::Clock, Sysvar},
 };
 use spl_token::{self, state::Account as SplTokenAccount, instruction as spl_token_instruction};
 use borsh::{BorshDeserialize, BorshSerialize};
 
-// Constants for the dual phase pricing algorithm
+// Constants for the dual-phase pricing algorithm.
 const DEFAULT_CURRENT_VOLUME: f64 = 10.0;
 const DEFAULT_AVERAGE_VOLUME: f64 = 7.0;
 const DEFAULT_TIME_SINCE_LAST_TRADE: f64 = 1.0;
 
+/// Represents a shareholder account with ownership and balance details.
 #[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize)]
 pub struct ShareAccount {
     pub owner: Pubkey,
     pub balance: u64,
 }
 
+/// Custom errors to represent specific failure reasons in the FriendTech program.
 pub enum FriendtechError {
     IncorrectOwner,
     InsufficientFunds,
@@ -32,11 +32,13 @@ impl From<FriendtechError> for ProgramError {
     }
 }
 
+/// Instructions supported by the FriendTech program, including buying and selling of shares.
 pub enum FriendtechInstruction {
     BuyShares { amount: u64 },
     SellShares { amount: u64 },
 }
 
+/// Calculate base price derived from the number of current holders.
 fn base_price_from_holders(current_holders: u32) -> f64 {
     if current_holders <= 10 {
         0.1 * current_holders as f64
@@ -45,22 +47,24 @@ fn base_price_from_holders(current_holders: u32) -> f64 {
     }
 }
 
+/// Dual-phase pricing algorithm considering trading volume, 
+/// number of current holders, and the time elapsed since the last trade.
 fn dual_phase_pricing(current_holders: u32, current_volume: f64, average_volume: f64, time_since_last_trade: f64) -> f64 {
-    let volume_adjustment_factor = 0.01;
-    let inactivity_adjustment_factor = 0.005;
-    let inactivity_threshold = 24.0;
+    const VOLUME_ADJUSTMENT_FACTOR: f64 = 0.01;
+    const INACTIVITY_ADJUSTMENT_FACTOR: f64 = 0.005;
+    const INACTIVITY_THRESHOLD: f64 = 24.0;
 
     let base_price = base_price_from_holders(current_holders);
-
     let volume_ratio = current_volume / average_volume;
 
-    if time_since_last_trade > inactivity_threshold {
-        base_price * (1.0 - inactivity_adjustment_factor)
+    if time_since_last_trade > INACTIVITY_THRESHOLD {
+        base_price * (1.0 - INACTIVITY_ADJUSTMENT_FACTOR)
     } else {
-        base_price * (1.0 + volume_adjustment_factor * volume_ratio)
+        base_price * (1.0 + VOLUME_ADJUSTMENT_FACTOR * volume_ratio)
     }
 }
 
+/// Main entry point for processing instructions related to the FriendTech program.
 entrypoint!(process_instruction);
 fn process_instruction(
     program_id: &Pubkey,
@@ -133,31 +137,28 @@ fn process_instruction(
     Ok(())
 }
 
+/// Tests to validate the dual-phase pricing algorithm's logic and outcomes.
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_dual_phase_pricing() {
-        // For the scenario when current_holders = 5, 
-        // current_volume = 10.0, average_volume = 7.0, time_since_last_trade = 1.0
-        let base_price = base_price_from_holders(5); // 0.5
+        // Test the pricing algorithm with a set of predefined scenarios.
+
+        let base_price = base_price_from_holders(5); // Expected to be 0.5
         let volume_ratio = 10.0 / 7.0;
-        let volume_adjustment_factor = 0.01;
-        let expected = base_price * (1.0 + volume_adjustment_factor * volume_ratio);
+        let expected = base_price * (1.0 + 0.01 * volume_ratio);
         assert_eq!(dual_phase_pricing(5, 10.0, 7.0, 1.0), expected);
 
-        // For the scenario with inactivity over the threshold
         assert_eq!(dual_phase_pricing(5, 10.0, 7.0, 25.0), base_price * (1.0 - 0.005));
 
-        // For a scenario with more than 10 current holders
-        let base_price_high = base_price_from_holders(15); // 6.0
-        let expected_high = base_price_high * (1.0 + volume_adjustment_factor * volume_ratio);
+        let base_price_high = base_price_from_holders(15); // Expected to be 6.0
+        let expected_high = base_price_high * (1.0 + 0.01 * volume_ratio);
         assert_eq!(dual_phase_pricing(15, 10.0, 7.0, 1.0), expected_high);
 
-        // For a scenario with exactly 10 current holders
-        let base_price_exact = base_price_from_holders(10); // 1.0
-        let expected_exact = base_price_exact * (1.0 + volume_adjustment_factor * volume_ratio);
+        let base_price_exact = base_price_from_holders(10); // Expected to be 1.0
+        let expected_exact = base_price_exact * (1.0 + 0.01 * volume_ratio);
         assert_eq!(dual_phase_pricing(10, 10.0, 7.0, 1.0), expected_exact);
     }
 }
